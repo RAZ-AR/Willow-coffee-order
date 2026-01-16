f0cfa2c11d9f93d5d65990c2654ac9c4adbf4210
RAZ-AR <razmikovich@gmail.com>
2026-01-16 17:16:10 +0100

    commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const BRAND = { name: "Willow", accent: "#14b8a6" };
-export const BACKEND_URL = "https://script.google.com/macros/s/AKfycbztt8YDXYTAb9xPOSVF5ivBoSc9IJCiywY5iQWBDxPMlBuJ10XhGnoPycVwp0lhshMY/exec";
+export const BACKEND_URL = "https://script.google.com/macros/s/AKfycbza5kOuztZ0yABRLXnyNM4G9qtQq8gCzEKWaadq4TlwRJ1S9ITyUW3nAzwTeSI_ATM2/exec";
 export const SHEET_JSON_URLS = {
     menu: "https://opensheet.elk.sh/1DQ00jxOF5QnIxNnYhnRdOqB9DXeRLB65L3eF6pSQMHw/MENU",
     ads: "https://opensheet.elk.sh/1DQ00jxOF5QnIxNnYhnRdOqB9DXeRLB65L3eF6pSQMHw/ADS",
